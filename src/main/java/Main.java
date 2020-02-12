25b72cf4e52cc2128081a54a1f0d912c64134699
Da Li <lind@lida1992.lan>
2020-02-12 14:56:51 +0800

    first commit of road recovery v1.2

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO: get inputs and pre-process
+
+
+        //TODO: execute the algorithm
+
+
+        //TODO: flush outputs
+
+
+    }
+}
