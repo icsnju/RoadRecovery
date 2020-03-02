fe49a2dc8a03d797eb1368096828f75f29378dc9
Da Li <njulida@outlook.com>
2020-03-02 10:16:54 +0800

    automatic compression.

--- compress.sh
+++ compress.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 cd src/test/resources/
-zip -r outputs-02-29-47.zip outputs/
+zip -r outputs-47-0302.zip outputs/
 git status
-git add -f outputs-02-29-47.zip
+git add -f outputs-47-0302.zip
 git commit -am "automatic compression."
 git push -u origin master
