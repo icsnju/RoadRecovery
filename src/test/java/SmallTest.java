e17cfdf395fac02dd8086ca9c38114856367c180
Da Li <njulida@outlook.com>
2020-03-03 20:39:07 +0800

    add new test cases (48-51)

--- src/test/java/SmallTest.java
+++ src/test/java/SmallTest.java
@@ -11,8 +11,8 @@ import java.io.PrintWriter;
 
 public class SmallTest {
 
-    private int testBegin = 10201;
-    private int testCount = 100;
+    private int testBegin = 10048;
+    private int testCount = 4;
 
     private static PrintWriter writer = null;
     private static final Graph graph;
@@ -37,7 +37,7 @@ public class SmallTest {
             System.out.println("\nCase " + testIndex + ":");
             PathSet pathSet = new PathSet();
             pathSet.readAll2Path(graph, testIndex, writer, false,
-                    "src/test/resources/test-data-10201-10300.txt", null);
+                    "src/test/resources/test-data-10048-10051.txt", null);
 
             //execute algorithm
             nju.ics.Algorithm.DPAlgorithm algorithm = new DPAlgorithm();
